d5af1eba4b302cf5c58134d07927a3c6ca858f16
Dewi Nurdin <dewinurdin@gmail.com>
2017-12-26 22:24:32 -0600

    added basic html and css

---
+++ authorization_code/public/javascript/firebase.js
@@ -0,0 +1,12 @@
+console.log("Hello firebase");
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAUTVIlZcanGG_da_gpG4qelJWEK_9aibQ",
+    authDomain: "spotify-project-58959.firebaseapp.com",
+    databaseURL: "https://spotify-project-58959.firebaseio.com",
+    projectId: "spotify-project-58959",
+    storageBucket: "",
+    messagingSenderId: "308746784133"
+  };
+  firebase.initializeApp(config);
+</script>
