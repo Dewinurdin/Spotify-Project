b68e61ba680aa179ccec2883b9361f5e656d2c36
dewinurdin <dewinurdin@gmail.com>
2018-04-29 03:00:44 -0500

    still trying to fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(express.static("authorization_code/public/default.html"));
+app.use(express.static("authorization_code/public"));
 
 const PORT = process.env.PORT || 5000;
 
